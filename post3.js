5d2a02e7d6bf268ec1fde8a0ae3c15473e2f8c55
marsksatriagotham <48630776+marsksatriagotham@users.noreply.github.com>
2019-03-17 04:01:41 +0700

    Add files via upload

--- post3.js
+++ post3.js
@@ -18,13 +18,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*4)+ 1);
-var xxx=null;
-if(x=="1"){xxx="do-you-need-college-health-insurance.html"}
-if(x=="2"){xxx="protection-during-golden-years-health.html"}
-if(x=="3"){xxx="traveling-alternative-roads-other.html"}
-if(x=="4"){xxx="what-you-dont-know-can-kill-you-health.html"}a_to_ve[i].href="https://antinyinyir.blogspot.com/2019/03/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="https://antinyinyir.blogspot.com/2019/03/traveling-alternative-roads-other.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
